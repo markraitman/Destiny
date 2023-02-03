687fa0e8a0877740ec44eccb537bb1488f397526
Mark Raitman <markraitman@mail.ru>
2023-02-03 22:07:27 +0300

    Создан проект Destiny по урокам курса Анжелы с учебного портала Udemy, добавлены картинки для главного экрана, иконки и изображение лоунч-скрина, а также созданы файлы проекта на архитектуре MVC

---
+++ Destiny/Controller/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Destiny
+//
+//  Created by Марк Райтман on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    //MARK: Outlets
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    //MARK: Actions
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
+    
+    //MARK: Methods
+
+}
+
---
+++ Destiny/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destiny
+//
+//  Created by Марк Райтман on 03.02.2023.
+//
+
+import Foundation
