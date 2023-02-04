32fd996b7c134c20c2ae6d6eb73aa3c4a93fa660
Mark Raitman <markraitman@mail.ru>
2023-02-04 15:32:30 +0300

    Добавлена архитектура MVC, модель StoryBrain и методы опции выбора

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -10,22 +10,14 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: Properties
+    var storyBrain = StoryBrain()
     
-    let stories = [
-        Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
-        Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
-        Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
-    ]
-    
-    var storyNumber = 0
 
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = stories[0].title
-        choice1Button.setTitle(getChoice1(), for: .normal)
-        choice2Button.setTitle(getChoice2(), for: .normal)
         
+        updateUI()
     }
     
     //MARK: Outlets
@@ -35,25 +27,16 @@ class ViewController: UIViewController {
     
     //MARK: Actions
     @IBAction func choiceMade(_ sender: UIButton) {
-        if choice1Button.titleLabel?.text == "Take a left" {
-            storyLabel.text = "You see a tiger"
-        }
-        else if choice2Button.currentTitle == "Take a right" {
-            storyLabel.text = "You find a treasure chest"
-        }
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
     }
     
     //MARK: Methods
-    func getChoice1() -> String {
-        return stories[storyNumber].choice1
-        }
-    
-    func getChoice2() -> String {
-        return stories[storyNumber].choice2
-        }
-    
-    func getStoryTitle() -> String {
-        return stories[storyNumber].title
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
 }
 
--- Destiny/Model/Story.swift
+++ Destiny/Model/Story.swift
@@ -10,12 +10,7 @@ import Foundation
 struct Story {
     var title: String
     var choice1: String
+    var choice1Destination: Int
     var choice2: String
-    
-    init(title: String, choice1: String, choice2: String) {
-        self.title = title
-        self.choice1 = choice1
-        self.choice2 = choice2
-    }
-    
+    var choice2Destination: Int
 }
